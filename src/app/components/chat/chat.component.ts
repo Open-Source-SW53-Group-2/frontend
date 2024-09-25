26f76f6efce5f075919009adf09b06de78d39d35
Alessandro David <alessandrodp2222@gmail.com>
2024-09-25 15:03:24 -0500

    feat: chat design added

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {ChatService} from "../../services/chat.service";
+import {ChatMessage} from "../../models/chat-message";
+
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.css'
+})
+export class ChatComponent implements OnInit {
+
+  constructor(private chatService: ChatService) {
+  }
+
+  ngOnInit(): void {
+    this.chatService.joinRoom("Driver Chat");
+  }
+
+  sendMessage(): void {
+    const chatMessage = {
+      message: 'HOLA',
+      user: '1'
+    } as ChatMessage;
+    this.chatService.sendMessage("Driver Chat", chatMessage)
+  }
+
+}

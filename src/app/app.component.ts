155a13933f53bf88b15b309059b2aa8d6fcf511c
SamuelValeraGarces <u202111952@upc.edu.pe>
2024-09-26 11:40:01 -0500

    modify app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {SocialLoginComponent} from "./components/social-login/social-login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, SocialLoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

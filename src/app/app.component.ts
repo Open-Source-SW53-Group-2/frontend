a99f47b10ec390c67c3b6fdcdf59cffbdd802ca1
FrankyMallmaE <u20211c250>
2024-09-26 10:20:29 -0500

    feat: add the confirmation of reservations view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TravelBookingComponent} from "./confirmation-reservations/components/travel-booking/travel-booking.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TravelBookingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

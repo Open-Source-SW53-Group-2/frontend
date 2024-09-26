2b9f8f4293ab6f3000981cbcdb7036523006d9c7
Kaitozerob <u202223781@upc.edu.pe>
2024-09-26 11:29:02 -0500

    Add custom routes for API endpoints in json-server

---
+++ src/app/destination/services/destination-api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DestinationApiService {
+  API_URL:string = 'https://my-json-server.typicode.com/ericx2024/UniRiderDB/db';
+  constructor(private httpClient:HttpClient) { }
+
+  getService(): Observable<any>{
+    return this.httpClient.get(this.API_URL).pipe(res=>res);
+  }
+}
